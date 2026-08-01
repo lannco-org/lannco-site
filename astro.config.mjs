fd47789a7bb5da5518bf7b01ed4f1f7e2fb4dcd5
SashaZhou35 <sashazhoubgc@gmail.com>
2026-08-01 20:49:47 +0200

    Correct production site URL
    Point Astro production metadata at https://www.lann-co.com.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   // TODO: set to the client's production domain before launch
-  site: 'https://lannco.com',
+  site: 'https://www.lann-co.com',
 });
